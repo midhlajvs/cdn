bd2ebc49066f56db19fb2698ea92396508e9017b
midhlajvs <midhlaj.vs@gmail.com>
2019-06-19 19:17:02 +0530

    Create cdn_partition.sh

---
+++ cdn_partition.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+function usage() {
+
+echo """
+Enter the disks to partition at the prompt with space as delimiter, Example: sdb sdc sdd sdf
+The disks provided will be partitoned with a single partion of 90% in GPT
+"""
+
+}
+usage
+
+read -p "Enter the disks to partition: "  disks
+
+if [ -z "$disks" ]
+then
+
+ echo "No disks specified !!!!"
+ usage
+ exit 1
+
+fi
+
+function command_exist {
+  command -v "$@" > /dev/null 2>&1
+}
+
+if ! command_exist parted; then
+ apt-get install parted -y
+fi
+
+for i in $disks
+do
+ parted -s /dev/$i mklabel gpt
+ parted -s -a optimal /dev/$i mkpart primary 2048s 90%
+ echo "Partitioning /dev/"$i "completed"
+done
